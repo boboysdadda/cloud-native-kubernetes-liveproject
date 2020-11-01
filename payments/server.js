cba586635fd0b98177771a56f777915352c1df81
James D <james.dreier@tensure.io>
2020-11-01 12:12:27 -0700

    completed milestone 1

--- payments/server.js
+++ payments/server.js
@@ -3,7 +3,7 @@ const express = require("express")
 const pino = require('pino')
 
 const config = require("./config")()
-const logger = pino(pino.destination(config.log_file))
+const logger = pino(pino.destination())
 
 const app = express();
 
@@ -19,3 +19,34 @@ const server_port = config.server_port
 app.listen(server_port, () => {
     logger.info(`Server is running on port ${server_port}.`)
 })
+
+const health = require('@cloudnative/health-connect')
+let healthCheck = new health.HealthChecker();
+const livePromise = () => new Promise((resolve, _reject) => {
+
+  const appFunctioning = true;
+
+  // You should change the above to a task to determine if your app is functioning correctly
+
+  if (appFunctioning) {
+    logger.info('Im alive');
+    resolve();
+
+  } else {
+    logger.error('Im dead');
+    reject(new Error("App is not functioning correctly"));
+  }
+
+});
+
+let liveCheck = new health.LivenessCheck("LivenessCheck", livePromise);
+
+healthCheck.registerLivenessCheck(liveCheck);
+let readyCheck = new health.PingCheck(`localhost`,`3000`)
+
+healthCheck.registerReadinessCheck(readyCheck);
+
+app.use('/live', health.LivenessEndpoint(healthCheck));
+
+app.use('/ready', health.ReadinessEndpoint(healthCheck));
+
